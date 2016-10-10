998f99cf6accf25e098f39919cd92f110552a44c
Asmins <admin@MacBook-Air-Asmins.local>
2016-10-10 10:29:39 +0300

    Some fix,and prepare for viper

--- ViperProject/LoginViewController.swift
+++ ViperProject/LoginViewController.swift
@@ -12,21 +12,27 @@ class LoginViewController: UIViewController {
 
     @IBOutlet var textField: UITextField!
     @IBOutlet var passWordTextField: UITextField!
-    
+    var bool:Bool!
     var realmManager = RealmModel()
-    var array = [UserModel]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
+    
     @IBAction func dissmisToRootView(_ sender: AnyObject) {
         self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func logibButton(_ sender: AnyObject) {
         self.realmManager.getUserFromDataBase()
-        array = self.realmManager.result
-        self.realmManager.login(email: textField.text!, password: passWordTextField.text!, data: array)
-   }  
+        bool =  self.realmManager.login(email: textField.text!, password: passWordTextField.text!,viewController:self)
+        
+        if bool == true {
+            let controller = self.storyboard?.instantiateViewController(withIdentifier: "HomeView") as! HomeViewController
+            self.navigationController?.pushViewController(controller, animated: true)
+        }else{
+            print("Please check you eMail or password")
+        }
+    }
 }
--- ViperProject/RealmModel.swift
+++ ViperProject/RealmModel.swift
@@ -14,6 +14,7 @@ class RealmModel {
     let user = UserModel()
     let realm = try! Realm()
     var result = [UserModel]()
+    var boolCheck = false
     
     func writeInDB(userName:String,password:String,email:String) {
         try! realm.write {
@@ -28,18 +29,26 @@ class RealmModel {
         result.append(contentsOf: realm.objects(UserModel.self))
     }
     
-    func login(email:String,password:String,data:[UserModel]) {
-        for i in 0..<data.count {
-            if email == data[i].email {
+    func login(email:String,password:String,viewController:UIViewController) -> Bool{
+        for i in 0..<result.count {
+            if email == result[i].email {
                 print("Same email")
-                if password == data[i].passwordUser {
+                if password == result[i].passwordUser {
                     print("Same password")
+                    boolCheck = true
                     break
+                }else{
+                    let alert = UIAlertController(title: "Error", message: "Check your email or password", preferredStyle: .alert)
+                    let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                    alert.addAction(ok)
+                    viewController.present(alert, animated: true, completion: nil)
+                    boolCheck = false
                 }
             }else{
                 print("It is not same ")
+                boolCheck = false
             }
         }
+        return boolCheck
     }
-    
 }
